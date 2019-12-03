05b1fc896c0c9d410dccc503f1eee0cb443e1ad8
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2019-12-03 11:11:35 -0500

    finished

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ rm OpenData_TTC_Schedules.zip
 virtualenv -p python3 env
 source env/bin/activate
 pip install -r requirements.txt
-mkdir data
 printf "\nEnter your Google Maps API key.\n"
 read api_key
 echo "API_KEY=\"$api_key\"" > keys.py
