6841c551c845deb31a500206f0409b9df57edfc0
Daniel Tamming <daniel.tamming@uwaterloo.ca>
2020-03-28 10:47:15 -0400

    added to gitignore

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# downloads the transit data from the TTC
 wget http://opendata.toronto.ca/TTC/routes/OpenData_TTC_Schedules.zip
 unzip OpenData_TTC_Schedules.zip -d OpenData_TTC_Schedules
 rm OpenData_TTC_Schedules.zip
+# create a virtualenvironment with the required packages installed
 virtualenv -p python3 env
 source env/bin/activate
 pip install -r requirements.txt
+# saves your google maps api key
 printf "\nEnter your Google Maps API key.\n"
 read api_key
 echo "API_KEY=\"$api_key\"" > keys.py
